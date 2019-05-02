4d3b422e1ee8da18511c608f028364c5a6a76e4e
kanitson <dargonpee@gmail.com>
2019-05-02 10:12:09 -0700

    Add generate jwt.

--- routes/user.js
+++ routes/user.js
@@ -3,50 +3,84 @@ const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 
 router.get("/", (req, res) => {
-    res.send({
-        status: "user"
-    });
+  res.send({
+    status: "user"
+  });
+});
+router.get("/list/:pageNumber", (req, res) => {
+  const pageNumber = req.params.pageNumber;
+  let itemPerPage = 5;
+  MongoClient.connect(
+    "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true",
+    {
+      useNewUrlParser: true
+    },
+    function(err, db) {
+      if (err) {
+        res.sendStatus(404);
+        return;
+      }
+      let dbo = db.db("newDatabase62");
+      dbo
+        .collection("userLoginTable")
+        .find(
+          {},
+          {
+            limit: itemPerPage,
+            skip: itemPerPage * (Number(pageNumber) - 1),
+            projection: {
+              _id: 1,
+              rank: 1,
+              first_name: 1
+            }
+          }
+        )
+        .sort({
+          _id: -1
+        })
+        .toArray(function(err, result) {
+          if (err) {
+            res.sendStatus(404);
+          } else {
+            res.send(result);
+          }
+          db.close();
+        });
+    }
+  );
 });
 
-router.get('/list/:pageNumber', (req, res) => {
-    const pageNumber = req.params.pageNumber;
-    console.log(pageNumber);
-
-    MongoClient.connect(
-        "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true", {
-            useNewUrlParser: true
-        },
-        function (err, db) {
-            if (err) {
-                res.sendStatus(404);
-                return;
-            }
-            let dbo = db.db("newDatabase62");
-            dbo
-                .collection('userLoginTable')
-                .find({}, {
-                    limit: 10,
-                    skip: 10 * (Number(pageNumber) - 1),
-                    projection: {
-                        _id: 0,
-                        first_name: 1,
-                        last_name: 1,
-                        username: 1
-                    }
-                })
-                .sort({
-                    _id: -1
-                })
-                .toArray(function (err, result) {
-                    if (err) {
-                        res.sendStatus(404);
-                    } else {
-                        res.send(result);
-                    }
-                    db.close();
-                });
-        }
-    );
+router.get("/list-count", (req, res) => {
+  MongoClient.connect(
+    "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true",
+    {
+      useNewUrlParser: true
+    },
+    function(err, db) {
+      if (err) {
+        res.sendStatus(404);
+        return;
+      }
+      let dbo = db.db("newDatabase62");
+      dbo
+        .collection("userLoginTable")
+        .find({})
+        .count(function(err, result) {
+          if (err) {
+            console.log(err);
+            res.sendStatus(404);
+          }
+          if (result) {
+            res.send({
+              total_item: result
+            });
+          } else {
+            res.sendStatus(404);
+          }
+          db.close();
+        });
+    }
+  );
 });
 
 module.exports = router;
